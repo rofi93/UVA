856fc51b88d14b4c36e60be0a6a85d20782d388c
rofi93 <khairul.basar93@gmail.com>
2018-09-17 12:49:22 +0600

    new solved problems

---
+++ 10298.cpp
@@ -0,0 +1,176 @@
+/* -------------------------------- */
+/* Name: MD. Khairul Basar          */
+/* Institute: HSTU                  */
+/* Dept: CSE                        */
+/* Email: khairul.basar93@gmail.com */
+/* -------------------------------- */
+
+#include <bits/stdc++.h>
+/* all header files */
+
+#define fs            first
+#define sc            second
+#define sp            printf(" ")
+#define nl            printf("\n")
+#define pb(a)         push_back(a)
+
+#define setzero(a)    memset(a,0,sizeof(a))
+#define setneg(a)     memset(a,-1,sizeof(a))
+#define setinf(a)     memset(a,126,sizeof(a))
+
+#define tc1(x)        printf("Case %d: ",x)
+#define tc2(x)        printf("Case #%d: ",x)
+#define tc3(x)        printf("Case %d:\n",x)
+#define tc4(x)        printf("Case #%d:\n",x)
+
+#define iin(x)        scanf("%d",&x)
+#define din(x)        scanf("%lf",&x)
+#define lin(x)        scanf("%lld",&x)
+#define clin(x)       scanf("%I64d",&x)
+
+#define pr1(x)        cout<<x<<"\n"
+#define pr2(x,y)      cout<<x<<" "<<y<<"\n"
+#define pr3(x,y,z)    cout<<x<<" "<<y<<" "<<z<<"\n"
+#define pr4(w,x,y,z)  cout<<w<<" "<<x<<" "<<y<<" "<<z<<"\n"
+#define fast          ios::sync_with_stdio(0)
+#define read          freopen("input.txt","r",stdin)
+#define write         freopen("output.txt","w",stdout)
+#define prflag1(flag) printf("%s\n",(flag)?"YES":"NO")
+#define prflag2(flag) printf("%s\n",(flag)?"Yes":"No")
+#define prflag3(flag) printf("%s\n",(flag)?"yes":"no")
+/* macro definitions */
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair<int, int>pii;
+typedef pair<LL, LL>pll;
+typedef vector<int>vi;
+typedef vector<LL>vll;
+typedef vector<pii>vpii;
+typedef vector<pll>vpll;
+/* type definitions */
+
+LL MOD_EXPO(LL b, LL p, LL m)
+{
+    if(p==0) return 1;
+    LL ret=MOD_EXPO(b,p/2,m)%m;
+    ret=(ret*ret)%m;
+    return ((p&1) ? (ret*b)%m : ret%m);
+}
+LL POWER(LL N, LL K)
+{
+    LL i,ans=1;
+    for(i=1; i<=K; i++) ans*=N;
+    return ans;
+}
+int SET(int N, int pos)
+{
+    return (N | (1<<pos));
+}
+int RESET(int N, int pos)
+{
+    return (N & !(1<<pos));
+}
+bool CHECK(int N, int pos)
+{
+    return (N & (1<<pos));
+}
+/* necessary functions */
+
+int dx4[]= {1,-1,0,0};
+int dy4[]= {0,0,1,-1};
+int dx6[]= {0,0,1,-1,0,0};
+int dy6[]= {1,-1,0,0,0,0};
+int dz6[]= {0,0,0,0,1,-1};
+int dx8[]= {1,-1,0,0,-1,1,-1,1};
+int dy8[]= {0,0,1,-1,1,1,-1,-1};
+int dkx8[]= {-1,1,-1,1,-2,-2,2,2};
+int dky8[]= {2,2,-2,-2,1,-1,1,-1};
+/* direction arrays */
+
+int tc=1;
+const double eps=1e-9;
+const double pi=acos(-1.0);
+const long long int mx=2e6;
+const long long int mod=1e9+7;
+/* global declarations */
+
+char str[mx+5];
+string pattern, text;
+int back_table[mx+5];
+
+void pre_process(void)
+{
+    int i,j,n;
+
+    i=0;
+    j=-1;
+    n=text.size();
+    back_table[0]=-1;
+    while(i<n)
+    {
+        while(j>=0 && text[i]!=text[j])
+        {
+            j=back_table[j];
+        }
+        i++;
+        j++;
+        back_table[i]=j;
+    }
+    return;
+}
+
+bool kmp(void)
+{
+    int i,j,n,m;
+
+    i=0;
+    j=0;
+    n=text.size();
+    m=pattern.size();
+    while(i<n)
+    {
+        while(j>=0 && text[i]!=pattern[j])
+        {
+            return false;
+        }
+        i++;
+        j++;
+        if(j==m)
+        {
+            j=0;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int i,n,m;
+    while(scanf("%s",str))
+    {
+        text=str;
+        if(text==".") break;
+
+        pre_process();
+
+        pattern="";
+        n=text.size();
+        for(i=0; i<n; i++)
+        {
+            pattern+=text[i];
+            m=pattern.size();
+            if(n%m==0)
+            {
+                if(kmp())
+                {
+                    pr1(n/m);
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
+}
---
+++ 10810.cpp
@@ -0,0 +1,171 @@
+/* -------------------------------- */
+/* Name: MD. Khairul Basar          */
+/* Institute: HSTU                  */
+/* Dept: CSE                        */
+/* Email: khairul.basar93@gmail.com */
+/* -------------------------------- */
+
+#include <bits/stdc++.h>
+/* all header files */
+
+#define fs            first
+#define sc            second
+#define sp            printf(" ")
+#define nl            printf("\n")
+#define pb(a)         push_back(a)
+
+#define setzero(a)    memset(a,0,sizeof(a))
+#define setneg(a)     memset(a,-1,sizeof(a))
+#define setinf(a)     memset(a,126,sizeof(a))
+
+#define tc1(x)        printf("Case %d: ",x)
+#define tc2(x)        printf("Case #%d: ",x)
+#define tc3(x)        printf("Case %d:\n",x)
+#define tc4(x)        printf("Case #%d:\n",x)
+
+#define iin(x)        scanf("%d",&x)
+#define din(x)        scanf("%lf",&x)
+#define lin(x)        scanf("%lld",&x)
+#define clin(x)       scanf("%I64d",&x)
+
+#define pr1(x)        cout<<x<<"\n"
+#define pr2(x,y)      cout<<x<<" "<<y<<"\n"
+#define pr3(x,y,z)    cout<<x<<" "<<y<<" "<<z<<"\n"
+#define pr4(w,x,y,z)  cout<<w<<" "<<x<<" "<<y<<" "<<z<<"\n"
+#define fast          ios::sync_with_stdio(0)
+#define read          freopen("input.txt","r",stdin)
+#define write         freopen("output.txt","w",stdout)
+#define prflag1(flag) printf("%s\n",(flag)?"YES":"NO")
+#define prflag2(flag) printf("%s\n",(flag)?"Yes":"No")
+#define prflag3(flag) printf("%s\n",(flag)?"yes":"no")
+/* macro definitions */
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair<int, int>pii;
+typedef pair<LL, LL>pll;
+typedef vector<int>vi;
+typedef vector<LL>vll;
+typedef vector<pii>vpii;
+typedef vector<pll>vpll;
+/* type definitions */
+
+LL MOD_EXPO(LL b, LL p, LL m)
+{
+    if(p==0)
+        return 1;
+    LL ret=MOD_EXPO(b,p/2,m)%m;
+    ret=(ret*ret)%m;
+    return ((p&1) ? (ret*b)%m : ret%m);
+}
+LL POWER(LL N, LL K)
+{
+    LL i,ans=1;
+    for(i=1; i<=K; i++)
+        ans*=N;
+    return ans;
+}
+int SET(int N, int pos)
+{
+    return (N | (1<<pos));
+}
+int RESET(int N, int pos)
+{
+    return (N & !(1<<pos));
+}
+bool CHECK(int N, int pos)
+{
+    return (N & (1<<pos));
+}
+/* necessary functions */
+
+int dx4[]= {1,-1,0,0};
+int dy4[]= {0,0,1,-1};
+int dx6[]= {0,0,1,-1,0,0};
+int dy6[]= {1,-1,0,0,0,0};
+int dz6[]= {0,0,0,0,1,-1};
+int dx8[]= {1,-1,0,0,-1,1,-1,1};
+int dy8[]= {0,0,1,-1,1,1,-1,-1};
+int dkx8[]= {-1,1,-1,1,-2,-2,2,2};
+int dky8[]= {2,2,-2,-2,1,-1,1,-1};
+/* direction arrays */
+
+int tc=1;
+const double eps=1e-9;
+const double pi=acos(-1.0);
+const long long int mx=5e5;
+const long long int mod=1e9+7;
+/* global declarations */
+
+LL n;
+LL arr[mx+5];
+LL temp[mx+5];
+LL tree[mx+5];
+
+LL query(LL index)
+{
+    LL sum=0;
+    while(index>0)
+    {
+        sum+=tree[index];
+        index-=index&(-index);
+    }
+    return sum;
+}
+
+void update(LL index, LL value)
+{
+    while(index<=n)
+    {
+        tree[index]+=value;
+        index+=index&(-index);
+    }
+    return;
+}
+
+void compress(void)
+{
+    LL i;
+    for(i=1; i<=n; i++)
+        temp[i]=arr[i];
+    sort(temp+1, temp+n+1);
+    for(i=1; i<=n; i++)
+        arr[i]=lower_bound(temp+1, temp+n+1, arr[i])-temp;
+    return;
+}
+
+LL count_inversion(void)
+{
+    LL i, sum;
+
+    compress();
+
+    sum=0;
+    for(i=n; i>=1; i--)
+    {
+        sum+=query(arr[i]-1);
+        update(arr[i], 1);
+    }
+    return sum;
+}
+
+int main()
+{
+    LL i,q,l,r;
+    while(scanf("%lld",&n))
+    {
+        if(n==0)
+            break;
+        setzero(tree);
+        for(i=1; i<=n; i++)
+        {
+            iin(arr[i]);
+            arr[i]++;
+        }
+        pr1(count_inversion());
+    }
+
+    return 0;
+}
